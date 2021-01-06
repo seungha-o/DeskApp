b8f86e17a2c61a6eab271efffae3156f52be6193
Your Name <you@example.com>
2021-01-06 11:16:19 +0900

    datepicker dateformat update

--- wefer/src/main/java/com/kh/wefer/project/model/service/ProjectService.java
+++ wefer/src/main/java/com/kh/wefer/project/model/service/ProjectService.java
@@ -1,5 +1,16 @@
 package com.kh.wefer.project.model.service;
 
+import java.util.List;
+
+import com.kh.wefer.project.model.domain.Project;
+
 public interface ProjectService {
+
+
+	List<Project> selectList();
+
+	void projectInsert(Project p);
+
+
 
 }
--- wefer/src/main/java/com/kh/wefer/project/model/service/ProjectServiceImpl.java
+++ wefer/src/main/java/com/kh/wefer/project/model/service/ProjectServiceImpl.java
@@ -1,8 +1,30 @@
 package com.kh.wefer.project.model.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service("pService")
-public class ProjectServiceImpl {
+import com.kh.wefer.project.model.dao.ProjectDao;
+import com.kh.wefer.project.model.domain.Project;
 
+@Service("pService")
+public class ProjectServiceImpl implements ProjectService {
+	@Autowired
+	private ProjectDao pDao;
+
+	@Override
+	public List<Project> selectList() {
+		return pDao.selectList();
+	}
+
+	@Override
+	public void projectInsert(Project p) {
+		pDao.projectInsert(p);
+		
+	}
+	
+	
+	
+	
 }
